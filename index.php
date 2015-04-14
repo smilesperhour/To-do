41c58b92e5883151a7b82201ddec27cfa47af643
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-04-14 12:18:34 -0700

    video 2 creating basic structure

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Simple To-do</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	<div class="wrap">
+		
+	</div>
+</body>
+</html>
