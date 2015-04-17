037b5459147bc54ccf07922ade415f102cc9a1cf
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-04-16 22:43:00 -0700

    video 9 function: add task in index.php

--- index.php
+++ index.php
@@ -16,4 +16,24 @@
 	   </form>
 	 </div>
 </body>
+<script scr="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+	add_task();
+
+	function add_task(){
+		$('add-new-task'.submit(function() {
+			var new_task = $('add-new-task input[name=new-task]'.val();
+
+			if (new_task != '') {
+				$.post('includes/add-task.php', { task: new_task}, function(data) {
+					$(('add-new-task input[add-new-task]').val();)
+					$(data).appendTo['task-list ul'].hide().fadeIn();
+				});
+			}
+
+			return false;
+		});
+	}
+
+</script>
 </html>
