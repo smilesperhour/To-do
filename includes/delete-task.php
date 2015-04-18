ff5087c653416b24ffd16562d1fe6d1e9c12b667
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-04-17 19:25:40 -0700

    video 14

--- includes/delete-task.php
+++ includes/delete-task.php
@@ -1,4 +1,10 @@
 <?php 
-$task_id = strip_tags($_POST)['id']
+$task_id = strip_tags($_POST)['id']);
+require('connect.php');
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+
+if ($result = $mysqli->query("DELETE FROM tasks WHERE id='task_id'")) {
+	
+}
 
  ?>
